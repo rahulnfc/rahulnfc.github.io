f8a0bd5ba9226f380ac870b7d99a1b9aa3721b64
rahulnfc <rahulraghav093@gmail.com>
2021-01-28 14:42:44 +0530

    modified

--- js/contactme.js
+++ js/contactme.js
@@ -1,15 +1,59 @@
-$("#submit-form").submit((e)=>{
-    e.preventDefault()
-    $.ajax({
-        url:"https://script.google.com/macros/s/AKfycby0ymh9ajQt0btq8EOU4U6rmdFFoqHTy8LUVWEWEucZ1u-7cawKE0AGGw/exec",
-        data:$("#submit-form").serialize(),
-        method:"post",
-        success:function (response){
-            alert("Form submitted successfully")
-            window.location.reload()
-        },
-        error:function (err){
-            alert("Something Error")
-        }
-    })
+
+$(function(){
+    if($("#submit-form").length){
+        $("#submit-form").validate({
+            rules:{
+                Name:{
+                    required: true
+                },
+                Email:{
+                    required: true,
+                    email: true
+                },
+                Number:{
+                    required: true,
+                    number: true,
+                    minlength:10
+                },
+                Message:{
+                    required: true
+                }
+            }
+        })
+    }
+    else{
+        $("#submit-form").submit((e)=>{
+            e.preventDefault()
+            $.ajax({
+                url:"https://script.google.com/macros/s/AKfycby0ymh9ajQt0btq8EOU4U6rmdFFoqHTy8LUVWEWEucZ1u-7cawKE0AGGw/exec",
+                data:$("#submit-form").serialize(),
+                method:"post",
+                success:function (response){
+                    alert("Form submitted successfully")
+                    window.location.reload()
+                },
+                error:function (err){
+                    alert("Something Error")
+                }
+            })
+        })
+        
+    }
+
 })
+
+// $("#submit-form").submit((e)=>{
+//     e.preventDefault()
+//     $.ajax({
+//         url:"https://script.google.com/macros/s/AKfycby0ymh9ajQt0btq8EOU4U6rmdFFoqHTy8LUVWEWEucZ1u-7cawKE0AGGw/exec",
+//         data:$("#submit-form").serialize(),
+//         method:"post",
+//         success:function (response){
+//             alert("Form submitted successfully")
+//             window.location.reload()
+//         },
+//         error:function (err){
+//             alert("Something Error")
+//         }
+//     })
+// })
