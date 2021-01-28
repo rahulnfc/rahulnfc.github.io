d0dbf881c946a7ce1f912499ac9e28c102e4520e
rahulnfc <rahulraghav093@gmail.com>
2021-01-28 18:56:27 +0530

    modified

--- js/contactme.js
+++ js/contactme.js
@@ -1,10 +1,4 @@
 
-// jQuery.validator.addMethod("phone", function(phone_number, element) {
-//     phone_number = phone_number.replace(/\s+/g, "");
-//     return this.optional(element) || phone_number.length > 9 && 
-//     phone_number.match(/^(\+?1-?)?(\([2-9]\d{2}\)|[2-9]\d{2})-?[2-9]\d{2}-?\d{4}$/);
-// }, "Please specify a valid phone number");
-
     $("#submit-form").validate({
         rules:{
             Name:{
@@ -42,18 +36,3 @@
 });
         
 
-// $("#submit-form").submit((e)=>{
-//     e.preventDefault()
-//     $.ajax({
-//         url:"https://script.google.com/macros/s/AKfycby0ymh9ajQt0btq8EOU4U6rmdFFoqHTy8LUVWEWEucZ1u-7cawKE0AGGw/exec",
-//         data:$("#submit-form").serialize(),
-//         method:"post",
-//         success:function (response){
-//             alert("Form submitted successfully")
-//             window.location.reload()
-//         },
-//         error:function (err){
-//             alert("Something Error")
-//         }
-//     })
-// })
